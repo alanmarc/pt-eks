e3b02ab8cb27b28ee8cd84e4294905dc3f77dbbf
alanMar <alan.marcos@smartkode.mx>
2023-04-02 23:21:41 -0600

    peticiones api

--- front-eks/src/components/ChineseFood.vue
+++ front-eks/src/components/ChineseFood.vue
@@ -10,30 +10,53 @@
         </v-col>      
       </v-row>
     </v-container>
-  
       <div>
-        <CardFood
-          name_food="Chinese"
-          price="450"
-          description="A traditional Mexican dish made with chili peppers and chocolate"
-          category="Mexican"
-          food_id="1"
-          image="https://tipsparatuviaje.com/wp-content/uploads/2018/09/mole-comida-mexicana.jpg"
-        />
+          <CardFood v-for="res in response" :key="res.key"
+          :name_food="res.name_food"
+          :price="res.price"
+          :description="res.description"
+          :category="res.category"
+          :food_id="res.food_id"
+          :image="res.image"
+          />
       </div>
   
   </template>
   
   <script>
-  
+  //import PopularFood from "@/services/PopularFood.js";
   import CardFood from "@/components/CardFood.vue";
+  import axios from 'axios';
   export default {
-    name: 'ChineseFood',
-  
+    name: 'IndexPrincipal',
     data: () => {
-  
+      return {
+        response: []
+      }
     },
   
+    /*methods: {
+      async getFoods () {
+        const res = await PopularFood.getPopularFood()
+        this.response = res.data
+        console.log(this.response)
+      }
+    },*/
+  
+    mounted() {
+      axios.get('http://localhost:3000/foods')
+        .then(({data}) => {
+          console.log(data)
+          const filteredFoods = data.filter(food => {
+            return food.category.includes('Chinese');
+            });
+          this.response = filteredFoods
+        })
+        .catch(error => {
+          console.log(error)
+          // Aquí puedes manejar el error, por ejemplo mostrando un mensaje de error
+        })
+    },
     components: {
       CardFood,
     }
--- front-eks/src/components/MexicanFood.vue
+++ front-eks/src/components/MexicanFood.vue
@@ -10,30 +10,43 @@
         </v-col>      
       </v-row>
     </v-container>
-  
       <div>
-        <CardFood
-          name_food="Indian"
-          price="450"
-          description="A traditional Mexican dish made with chili peppers and chocolate"
-          category="Mexican"
-          food_id="1"
-          image="https://tipsparatuviaje.com/wp-content/uploads/2018/09/mole-comida-mexicana.jpg"
-        />
+          <CardFood v-for="res in response" :key="res.key"
+          :name_food="res.name_food"
+          :price="res.price"
+          :description="res.description"
+          :category="res.category"
+          :food_id="res.food_id"
+          :image="res.image"
+          />
       </div>
   
   </template>
   
   <script>
-  
   import CardFood from "@/components/CardFood.vue";
+  import axios from 'axios';
   export default {
-    name: 'MexicanFood',
-  
+    name: 'IndexPrincipal',
     data: () => {
-  
+      return {
+        response: []
+      }
     },
   
+    mounted() {
+      axios.get('http://localhost:3000/foods')
+        .then(({data}) => {
+          console.log(data)
+          const filteredFoods = data.filter(food => {
+            return food.category.includes('Mexican');
+            });
+          this.response = filteredFoods
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
     components: {
       CardFood,
     }
